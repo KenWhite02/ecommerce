2fa2d1e42d1689d6c8f3f5f6ec3665785ad25227
KenWhite02 <whiteken2002@gmail.com>
2020-10-20 21:08:11 +0300

    added image field

--- store/models.py
+++ store/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
